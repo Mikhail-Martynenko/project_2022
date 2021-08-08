fcaf0370fb93489e506c596519dd063b36701e4b
Mikhail-Martynenko <misha.martynenko8@yandex.ru>
2021-08-08 17:59:36 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import {Delaunay} from "d3-delaunay";
+
+
+    //ctx.clearRect(100,100,300,300);
+   // ctx.strokeRect(50,50,50,50);
+
+
+
+class App extends React.Component {
+  render() {
+    const { name } = this.props;
+    return (
+      <>
+        <h1>
+          Hello world
+        </h1>
+      </>
+    );
+  }
+}
+
+export default App;
